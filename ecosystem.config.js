167da7c0b8a148bc332a45141a7afd39d140f8c3
pufei <pufei@snqu.com>
2019-10-17 20:02:22 +0800

    环境

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -46,7 +46,7 @@ module.exports = {
       path: "/var/www/node-server/react-next-koa2-pm2", // 目标服务器部署地址
       // "post-setup": "mkdir build",
       "post-deploy":
-        "npm install && npm run build:pro && pm2 reload ecosystem.config.js --env_production production"
+        "npm install && npm run build:pro && pm2 reload ecosystem.config.js --env production"
     },
     development: {
       user: "root",
@@ -55,11 +55,7 @@ module.exports = {
       repo: "https://github.com/pufei007/react-next-koa2.git",
       path: "/var/www/node-server/react-next-koa2-dev",
       "post-deploy":
-        "npm install && npm run build && pm2 reload ecosystem.config.js",
-      env: {
-        NODE_ENV: "development",
-        PORT: 3366
-      }
+        "npm install && npm run build && pm2 reload ecosystem.config.js --env development",
     }
   }
 };
