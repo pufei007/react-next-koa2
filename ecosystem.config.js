9a5612f2a991971d8c5dc2866f5f13e8f345c43a
pufei <pufei@snqu.com>
2019-10-17 17:57:47 +0800

    修改package

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "react-nextkoa2",
+      name: "react-next-koa2",
       script: "./server.js",
       cwd: "./", // current workspace
       watch: [
@@ -17,9 +17,19 @@ module.exports = {
 
       instances: 2, // start 2 instances
       node_args: "--harmony",
+      // 测试服
       env: {
+        NODE_ENV: "development",
+        HOST: "0.0.0.0",
+        PORT: 3000
+      },
+      // 正式服
+      env_production: {
         NODE_ENV: "production",
-        PORT: 5999
+        HOST: "0.0.0.0",
+        PORT: 3300
+        // 静态cdn地址
+        // PUBLIC_URL: '//static.yay.com.cn/yuema'
       },
       out_file: "./logs/out.log", // normal log
       error_file: "./logs/err.log", // error log
@@ -34,20 +44,23 @@ module.exports = {
       ref: "origin/master",
       repo: "https://github.com/pufei007/react-next-koa2.git",
       path: "/var/www/node-server/react-next-koa2", // 目标服务器部署地址
+      "post-setup": "mkdir build",
       "post-deploy":
-        "cnpm install && pm2 reload ecosystem.config.js  production"
+        "rm -rf buildBak && mv build buildBak && cnpm && cnpm install && npm run build:pro && pm2 reload ecosystem.config.js --env production"
     },
-    dev: {
-      user: "osboxes",
-      host: "192.168.1.186",
+    development: {
+      user: "root",
+      host: "148.70.131.200",
       ref: "origin/master",
-      repo: "https://github.com/choelea/image-utils-web.git",
-      path: "/home/osboxes/temp",
-      "post-deploy": "npm install && pm2 reload ecosystem.config.js --env dev",
-      env: {
-        NODE_ENV: "dev",
-        PORT: 3012
-      }
+      repo: "https://github.com/pufei007/react-next-koa2.git",
+      path: "/var/www/node-server/react-next-koa2",
+      "post-setup": "mkdir build",
+      "post-deploy":
+        "rm -rf buildBak && mv build buildBak && cnpm && cnpm install && npm run build && pm2 reload ecosystem.config.js",
+      // env: {
+      //   NODE_ENV: "dev",
+      //   PORT: 3012
+      // }
     }
   }
 };
